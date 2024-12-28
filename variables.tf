3293c366bb4570a9fb1497620d9889833ccdff27
sanjuthere <sanjuthere.in.gmail.com>
2024-12-28 13:07:20 +0530

    variables file

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  default = "10.0.2.0/24"
+}
+
+variable "key_name" {
+  description = "Key pair for SSH access"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
